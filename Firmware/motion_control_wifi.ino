d947ab6665a738519aa8467c0c338c2e6e89c3bc
Yousryy22 <121568220+Yousryy22@users.noreply.github.com>
2025-01-29 08:36:07 +0200

    Update motion_control_wifi.ino

--- Firmware/motion_control_wifi.ino
+++ Firmware/motion_control_wifi.ino
@@ -1,51 +1,125 @@
-// used libs: ESP32Servo , ros_lib/rosserial
+//rosrun rosserial_python serial_node.py _port:=/dev/ttyUSB0 _baud:=115200
 
+
+#include <ESP32Servo.h>
 #include <ros.h>
 #include <std_msgs/String.h>
-#include <ESP32Servo.h>  
-#include <WiFi.h>
 
-const char* ssid = "";
-const char* password = "";
+// Servo objects for the robot's limbs (arms and legs)
+Servo left_leg_upper, left_leg_middle, left_foot, left_leg_rotation;
+Servo right_leg_upper, right_leg_middle, right_foot, right_leg_rotation;
 
+// ROS node handle
 ros::NodeHandle nh;
-Servo servo1;  //test
 
-void commandCallback(const std_msgs::String &msg);
+// Movement functions
+void moveForward() {
+  left_leg_rotation.write(56);
+  right_leg_rotation.write(100);
+  left_leg_upper.write(55);
+  right_leg_upper.write(125);
+  delay(350);
+  left_leg_rotation.write(78);
+  right_leg_rotation.write(78);
+  left_leg_upper.write(60);
+  right_leg_upper.write(120);
+  delay(500);
+}
 
-ros::Subscriber<std_msgs::String> sub("motion_command", &commandCallback);
+void moveLeft() {
+  left_leg_upper.write(65);
+  right_leg_upper.write(112);
+  left_leg_rotation.write(0);
+  right_leg_rotation.write(0);
+  delay(500);
+  left_leg_upper.write(70);
+  right_leg_upper.write(108);
+  right_leg_rotation.write(90);
+  left_leg_rotation.write(90);
+  delay(1000);
+}
 
-void setup() {   
-    Serial.begin(115200); 
+void moveRight() {
+  left_leg_upper.write(65);
+  right_leg_upper.write(112);
+  left_leg_rotation.write(140);
+  right_leg_rotation.write(140);
+  delay(800);
+  left_leg_upper.write(70);
+  right_leg_upper.write(108);
+  right_leg_rotation.write(90);
+  left_leg_rotation.write(90);
+  delay(800);
+}
 
-    //connect to wifi
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
-    }
-    Serial.println("Connected to WiFi!");
-    Serial.println(WiFi.localIP()); // hnhtag el esp32's IP da b3den
+void moveReverse() {
+  for (int i = 0; i < 2; i++) {
+    left_leg_upper.write(65);
+    right_leg_upper.write(112);
+    left_leg_rotation.write(0);
+    right_leg_rotation.write(0);
+    delay(800);
+    left_leg_upper.write(70);
+    right_leg_upper.write(108);
+    right_leg_rotation.write(90);
+    left_leg_rotation.write(90);
+    delay(800);
+  }
+}
 
-    //test
-    servo1.attach(18,500,2400); 
+// Callback function for the servo_motion topic
+void servoMotionCallback(const std_msgs::String& msg) {
+  if (strcmp(msg.data, "forward") == 0) {
+    moveForward();
+  } else if (strcmp(msg.data, "left") == 0) {
+    moveLeft();
+  } else if (strcmp(msg.data, "right") == 0) {
+    moveRight();
+  } else if (strcmp(msg.data, "reverse") == 0) {
+    moveReverse();
+  }
+}
 
-    nh.initNode();
-    nh.subscribe(sub);
+// ROS subscriber
+ros::Subscriber<std_msgs::String> sub("servo_motion", &servoMotionCallback);
+
+void setup() {
+  // Initialize servos
+  Serial.begin(115200);
+  nh.getHardware()->setBaud(115200);
+  left_leg_upper.attach(33);
+  left_leg_middle.attach(25);
+  left_foot.attach(26);
+  left_leg_rotation.attach(32);
+  right_leg_upper.attach(21);
+  right_leg_middle.attach(19);
+  right_foot.attach(17);
+  right_leg_rotation.attach(16);
+  
+  left_leg_upper.write(90);
+  delay(50);
+  left_leg_middle.write(90);
+  delay(50);
+  left_foot.write(90);
+  delay(50);
+  left_leg_rotation.write(90);
+  delay(50);
+  right_leg_upper.write(90);
+  delay(50);
+  right_leg_middle.write(90);
+  delay(50);
+  right_foot.write(90);
+  delay(50);
+  right_leg_rotation.write(90);
+  delay(50);
+
+  // Initialize ROS node
+  nh.initNode();
+  nh.subscribe(sub);
 }
 
 void loop() {
-    nh.spinOnce();  //handle incoming mesages
-    delay(10);
-}
-
-void commandCallback(const std_msgs::String &msg) {
-    String command = msg.data; 
-
-    //test
-    if (command =="MOVE_SERVO") {
-        servo1.write(90);  
-    } else if (command=="RESET_SERVO") {
-        servo1.write(0);   
-    }
+  // Handle ROS callbacks
+  nh.spinOnce();
+  delay(1);
 }
